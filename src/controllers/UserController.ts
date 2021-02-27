092e02c6e8349ec3be167141931cf36c1057da3f
william ribeiro <william_jag_rs@hotmail.com>
2021-02-27 15:39:20 -0300

    nlw application finalized

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("Nome é obrigatório"),
+      email: yup.string().email().required("email inválido"),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error.errors);
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
 
@@ -13,7 +26,7 @@ class UserController {
     });
 
     if (userAlreadyExists) {
-      return response.status(400).json({ message: "User already exists!!" });
+      throw new AppError("User already exists!!");
     }
 
     const user = usersRepository.create({
